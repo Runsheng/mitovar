199ff2d3d99bdd44acb5dd57151e2905a265c8c4
runsheng <runsheng.lee@gmail.com>
2018-07-17 14:22:18 +0800

    add test

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mitovar',
+    version='V0.99',
+    packages=['test', 'script'],
+    url='github/runsheng/mitovar',
+    license='GPL-2',
+    author='runsheng',
+    author_email='runsheng.lee@gmail.com',
+    description='Mitochondrial DNA sequence assembly and annotation for nematodes using NGS data.'
+)
